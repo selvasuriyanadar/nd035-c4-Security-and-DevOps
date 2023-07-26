56fabcc1ca69920d68b247d9eee986a02cc58ab8
vignesh <selvasitpl@gmail.com>
2023-07-26 16:27:56 +0530

    deployable to tomcat

--- starter_code/src/main/java/com/example/demo/SareetaApplication.java
+++ starter_code/src/main/java/com/example/demo/SareetaApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SareetaApplication {
+public class SareetaApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SareetaApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SareetaApplication.class, args);
